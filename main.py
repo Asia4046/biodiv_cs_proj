83344b125b617370e040b0eb39dc01580eef97f8
Asia4046 <kirito234875@gmail.com>
2026-05-28 15:49:47 +0530

    fetch_all() fetch_where()

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import mysql.connector
 
+
+## MAIN BACKEND PROGRAM
 dataBase = mysql.connector.connect(
     host = "localhost",
     user = "root",
@@ -15,7 +17,7 @@ def create_db():
     try:
         dbCursor.execute("CREATE DATABASE biodiv_zoo")
     except:
-        print("Database exists!")
+        pass
 
 def create_table():
     biodiv_table = """
@@ -29,7 +31,7 @@ def create_table():
                     ORIGIN_COUNTRY VARCHAR(30) NOT NULL,
                     DIET_TYPE VARCHAR(30) NOT NULL,
                     ENDANGERED_RATE VARCHAR(30),
-                    FEEDING_QUANTITY VARCHAR(30) NOT NULL,
+                    FEEDING_QUANTITY INT NOT NULL,
                     HABITAT_TYPE VARCHAR(30) NOT NULL,
                     LIFE_SPAN_IN_YEARS INT NOT NULL,
                     BIRTH_RATE INT NOT NULL,
@@ -41,13 +43,87 @@ def create_table():
         dbCursor.execute("use biodiv_zoo")
         dbCursor.execute(biodiv_table)
     except:
-        print("Table Already Exists!")
+        pass
 
 def create_single_record():
-    pass
+    sql = "INSERT INTO ZOOMANAGE (SPECIES_ID, SPECIES_NAME, SCIENTIFIC_NAME, GENERIC_NAME, ZOO_COUNT, GLOBAL_COUNT, ORIGIN_COUNTRY, DIET_TYPE, ENDANGERED_RATE, FEEDING_QUANTITY, HABITAT_TYPE, LIFE_SPAN_IN_YEARS, BIRTH_RATE, DEATH_RATE)\
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+    spec_id = int(input("Enter Species ID(integer): "))
+    spec_name = input("Enter Species Name: ")
+    scien_name = input("Enter Scientific Name: ")
+    gen_name = input("Enter Generic Name: ")
+    zoo_count = int(input("Enter Zoo Count: "))
+    global_count = int(input("Enter Global Count: "))
+    origin = input("Enter Origin Country: ")
+    diet_type = input("Enter Diet Type: ")
+    endangered_status = input("Enter Endangered Status: ")
+    feed_qunt = int(input("Enter Feeding Quantity: "))
+    habitat_type = input("Enter Habitat Type: ")
+    life_span = int(input("Enter Life Span (in years): "))
+    birth_rate = int(input("Enter Birth Rate (no/year): "))
+    death_rate = input("Enter Death Rate Status: ")
+
+    val = (spec_id, spec_name, scien_name, gen_name, zoo_count, global_count, origin, diet_type, endangered_status, feed_qunt, habitat_type, life_span, birth_rate, death_rate)
+
+    try:
+        dbCursor.execute(sql, val)
+        print("success")
+    except:
+        print("error")
+    dataBase.commit()
 
 def create_multiple_records():
-    pass
+    data = []
+
+    sql = "INSERT INTO ZOOMANAGE (SPECIES_ID, SPECIES_NAME, SCIENTIFIC_NAME, GENERIC_NAME, ZOO_COUNT, GLOBAL_COUNT, ORIGIN_COUNTRY, DIET_TYPE, ENDANGERED_RATE, FEEDING_QUANTITY, HABITAT_TYPE, LIFE_SPAN_IN_YEARS, BIRTH_RATE, DEATH_RATE)\
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+    count = int(input("How many records do you want to enter?: "))
+    for i in range(count):
+        print("")
+        spec_id = int(input("Enter Species ID(integer): "))
+        spec_name = input("Enter Species Name: ")
+        scien_name = input("Enter Scientific Name: ")
+        gen_name = input("Enter Generic Name: ")
+        zoo_count = int(input("Enter Zoo Count: "))
+        global_count = int(input("Enter Global Count: "))
+        origin = input("Enter Origin Country: ")
+        diet_type = input("Enter Diet Type: ")
+        endangered_status = input("Enter Endangered Status: ")
+        feed_qunt = int(input("Enter Feeding Quantity: "))
+        habitat_type = input("Enter Habitat Type: ")
+        life_span = int(input("Enter Life Span (in years): "))
+        birth_rate = int(input("Enter Birth Rate (no/year): "))
+        death_rate = input("Enter Death Rate Status: ")
+        print("")
+
+        val = (spec_id, spec_name, scien_name, gen_name, zoo_count, global_count, origin, diet_type, endangered_status, feed_qunt, habitat_type, life_span, birth_rate, death_rate)
+        data.append(val)
+
+    try:
+        dbCursor.executemany(sql, data)
+        dataBase.commit()
+        print("Success")
+    except:
+        print("Error!")
+
+def fetch_all():
+    sql = "SELECT * FROM ZOOMANAGE"
+    dbCursor.execute(sql)
+
+    fetched_data = dbCursor.fetchall()
+    for i in fetched_data:
+        print(i)
+
+def fetch_where(field, op, payload):
+    sql = f"SELECT * FROM ZOOMANAGE WHERE {field} {op} {payload}"
+    dbCursor.execute(sql)
+
+    fetched_data = dbCursor.fetchall()
+    for i in fetched_data:
+        print(i)
+        
 
 create_db()
 create_table()
